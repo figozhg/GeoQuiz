be7679a9a42de9e0ed575e54d307472395c47263
figozhg <mobile.dev@foxmail.com>
2016-12-16 20:43:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bigzhg/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.example.bigzhg.geoquiz;
+
+/**
+ * Created by BigZhg on 2016/11/18.
+ */
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int testResId, boolean answerTrue) {
+        mTextResId = testResId;
+        mAnswerTrue = answerTrue;
+    }
+}
